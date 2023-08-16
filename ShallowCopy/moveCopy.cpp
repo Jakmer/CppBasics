11b7fb780625e103c885fb9761fa829ce858d3d4
Jakub Merta <kubamerta13@gmail.com>
2023-08-16 17:21:55 +0200

    Move constructor

--- ShallowCopy/moveCopy.cpp
+++ ShallowCopy/moveCopy.cpp
@@ -1,16 +1,53 @@
 #include <iostream>
+#include <vector>
 
 using std::cout;
 
 class A
 {
-    public:
-        int* data;
+public:
+    int *data;
 
-    A(int a){
+    A(int a)
+    {
         data = new int(a);
+        cout << "Created\n";
     }
 
-    A(const A &source):data
+    A(const A &source) : A(*(source.data))
+    {
+        cout << "Copied\n";
+    }
 
+    A(A &&source):data(source.data)
+    {
+        source.data=nullptr;
+        cout<<"Moved\n";
+    }
+
+    ~A()
+    {
+        delete data;
+        cout << "Destructed\n";
+    }
 };
+
+int main()
+{
+    std::vector<A> vec;
+    vec.push_back(A{10});
+    /*output without move constructors:
+        Created
+        Created
+        Copied
+        Destructed
+    */
+   /*with move constructor:
+        Created
+        Moved
+        Destructed
+        Destructed
+   */
+
+    return 0;
+}
