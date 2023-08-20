89bf50e41e5e226a94efdb43f8be97b04c55a8e6
Jakub Merta <kubamerta13@gmail.com>
2023-08-20 22:50:01 +0200

    Add test.cpp - simple file using smart pointers

---
+++ SmartPointers/test.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+
+class Test
+{
+private:
+    int data;
+
+public:
+    Test() : data{0} { std::cout << "\tTest constructor (" << data << ")" << std::endl; }
+    Test(int data) : data{data} { std::cout << "\tTest constructor (" << data << ")" << std::endl; }
+    int get_data() const { return data; }
+    ~Test() { std::cout << "\tTest destructor (" << data << ")" << std::endl; }
+};
+
+std::unique_ptr<std::vector<std::shared_ptr<Test>>> make();
+void fill(std::vector<std::shared_ptr<Test>> &vec, int num);
+void display(const std::vector<std::shared_ptr<Test>> &vec);
+
+int main()
+{
+    std::unique_ptr<std::vector<std::shared_ptr<Test>>> vec_ptr;
+    vec_ptr = make();
+    std::cout << "How many data points do you want to enter: ";
+    int num;
+    std::cin >> num;
+    fill(*vec_ptr, num);
+    display(*vec_ptr);
+    return 0;
+}
+
+std::unique_ptr<std::vector<std::shared_ptr<Test>>> make()
+{
+    return std::make_unique<std::vector<std::shared_ptr<Test>>>();
+}
+
+void fill(std::vector<std::shared_ptr<Test>> &vec, int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        int input{};
+        std::cin >> input;
+        std::shared_ptr<Test> ptr = std::make_shared<Test>(input);
+        vec.push_back(ptr);
+    }
+}
+
+void display(const std::vector<std::shared_ptr<Test>> &vec)
+{
+    std::cout << "Displaying vector\n";
+    for (const auto &i : vec)
+    {
+        std::cout << i->get_data() << '\n';
+    }
+}
