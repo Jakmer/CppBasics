ffd07b14d6ded94cef816f7daa3de819a4fe0f1c
Jakub Merta <kubamerta13@gmail.com>
2023-08-21 16:58:51 +0200

    Add BooleanManipulators.cpp

--- IO_Streams/BooleanManipulators.cpp
+++ IO_Streams/BooleanManipulators.cpp
@@ -1,1 +1,32 @@
 #include <iostream>
+#include <iomanip>
+
+using std::cout;
+
+int main()
+{
+    cout<<(1==1)<<'\n'; //  1
+    cout<<(1==0)<<'\n'; //  0
+
+    cout<<std::boolalpha;   //  changing to true/false
+
+    cout<<(1==1)<<'\n'; //  true
+    cout<<(1==0)<<'\n'; //  false
+
+    cout<<std::noboolalpha;   //  changing to 1/0
+
+    cout<<(1==1)<<'\n'; //  1
+    cout<<(1==0)<<'\n'; //  0
+
+    cout<<std::boolalpha;   //  changing to true/false 
+
+    cout<<(1==1)<<'\n'; //  true
+    cout<<(1==0)<<'\n'; //  false
+
+    cout<<std::resetiosflags(std::ios::boolalpha);   //  reset manipulator std::boolalpha
+
+    cout<<(1==1)<<'\n'; //  1
+    cout<<(1==0)<<'\n'; //  0
+
+    return 0;
+}
