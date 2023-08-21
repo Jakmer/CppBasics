e000ebe36ac094fa15e6d9f786c13853744fd3fb
Jakub Merta <kubamerta13@gmail.com>
2023-08-21 23:58:24 +0200

    Add input.txt InputFile.cpp

---
+++ IO_Streams/InputFile.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+int main()
+{
+    std::ifstream file;
+    file.open("/home/jakub/Documents/Workspace/C++/Course/IO_Streams/input.txt");
+
+    if (!file)
+    {
+        std::cerr << "Error: couldn't open file input.txt...\n";
+        return 1;
+    }
+
+    std::cout << std::setw(45) << std::setfill('=') << "" << std::endl<<std::setfill(' ');
+    std::cout << std::setw(15) << std::left << "COUNTRY" << std::setw(15) << std::left << "CAPITAL" << std::setw(15) << std::right << "POPULATION" << std::endl;
+    std::cout << std::setw(45) << std::setfill('=') << "" << std::endl<<std::setfill(' ');
+    std::string country, capital;
+    int population;
+    while (file >> country >> capital >> population)
+    {
+        std::cout << std::setw(15) << std::left << country << std::setw(15) << std::left << capital << std::setw(15) << std::right << population << std::endl;
+    }
+    file.close();
+    return 0;
+}
