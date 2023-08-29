457c5f0825ca484a9d34fccf54a27eccbb77e9b4
Jakub Merta <kubamerta13@gmail.com>
2023-08-29 13:41:11 +0200

    Update Deque.cpp

--- StandardTemplateLibrary/Deque.cpp
+++ StandardTemplateLibrary/Deque.cpp
@@ -15,7 +15,7 @@ void f1()
     std::deque<int> dq{1, 2, 3, 4, 5};
 
     display(dq);
-    std::cout<<dq.at(0)<<" "<<dq[1]<<'\n';
+    std::cout << dq.at(0) << " " << dq[1] << '\n';
 
     dq.pop_back();
     dq.push_back(6);
@@ -25,26 +25,48 @@ void f1()
     display(dq);
 }
 
-void f1()
+template <typename T>
+bool palindrome(std::deque<T> &dq)
 {
-    std::deque<int> dq{1, 2, 3, 4, 5};
-
-    display(dq);
-    std::cout<<dq.at(0)<<" "<<dq[1]<<'\n';
-
-    dq.pop_back();
-    dq.push_back(6);
-    dq.pop_front();
-    dq.push_front(0);
-
-    display(dq);
+    while (!dq.empty())
+    {
+        if (dq.front() != dq.back())
+        {
+            return false;
+        }
+        dq.pop_back();
+        dq.pop_front();
+    }
+    return true;
 }
 
+void isPalindrome(std::string &&str)
+{
+    std::deque<char> dq;
+    std::string tmp = str;
+    str.erase(std::remove_if(str.begin(), str.end(), ::isspace),
+              str.end());       //  removing whitespaces
+    std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+    for (const auto &i : str)
+    {
+        dq.push_back(i);
+    }
 
+    if (palindrome(dq))
+    {
+        std::cout << tmp << " is a palindrome\n";
+    }
+    else
+    {
+        std::cout << tmp << " is NOT a palindrome\n";
+    }
+}
 
 int main()
 {
     f1();
+    isPalindrome("A Santa at Nasa");
+    isPalindrome("Hello world");
 
     return 0;
 }
