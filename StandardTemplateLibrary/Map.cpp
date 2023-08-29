3a48377d157e96f52329c13bae7e0993521a22f5
Jakub Merta <kubamerta13@gmail.com>
2023-08-29 17:01:53 +0200

    Update Map.cpp

--- StandardTemplateLibrary/Map.cpp
+++ StandardTemplateLibrary/Map.cpp
@@ -2,12 +2,25 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <fstream>
+#include <string>
 
 template <typename T1, typename T2>
 void display(std::map<T1, T2> &m)
 {
     std::for_each(m.begin(), m.end(), [](auto x)
                   { std::cout << x.first << " " << x.second << '\n'; });
+}
+
+std::string clean_string(const std::string &s) {
+    std::string result;
+    for (char c: s) {
+        if (c == '.' || c == ',' || c == ';' || c == ':')
+            continue;
+        else
+            result += c;
+    }
+    return result;
 }
 
 void f1()
@@ -27,9 +40,34 @@ void f1()
         std::cout << it->first << ' ' << it->second << '\n';
 }
 
+void f2()
+{
+    std::ifstream file {"words.txt"};
+
+    if(!file)
+    {
+        std::cout<<"Cannot open file\n";
+    }
+
+    std::map<std::string,int> words;
+    std::string word;
+
+    while(file>>word)
+    {
+        word = clean_string(word);
+        words[word]++;
+    }
+
+    file.close();
+
+    display(words);
+
+}
+
 int main()
 {
     f1();
+    f2();
 
     return 0;
 }
