8ab7e02ce4f92ae91ccc7e93b712e31ba3d7c101
Jakub Merta <kubamerta13@gmail.com>
2023-08-29 17:29:15 +0200

    Add Stack.cpp Queue.cpp

---
+++ StandardTemplateLibrary/Queue.cpp
@@ -0,0 +1,30 @@
+#include <queue>
+#include <iostream>
+
+void f1()
+{
+    std::queue<int> s1;
+
+    for(int i : {13,12,3,7,21,9})
+    {
+        s1.push(i);
+    }
+
+    std::cout<<s1.front()<<" "<<s1.back()<<'\n';
+
+    s1.pop();
+    s1.push(50);
+
+    while(!s1.empty())
+    {
+        std::cout<<'['<<s1.front()<<"]\n";
+        s1.pop();
+    }
+    
+}
+
+int main()
+{
+    f1();
+    return 0;
+}
---
+++ StandardTemplateLibrary/Stack.cpp
@@ -0,0 +1,29 @@
+#include <stack>
+#include <iostream>
+
+void f1()
+{
+    std::stack<int> s1;
+
+    for(int i : {13,12,3,7,21,9})
+    {
+        s1.push(i);
+    }
+
+    std::cout<<s1.top()<<'\n';
+
+    s1.pop();
+    s1.push(50);
+
+    while(!s1.empty())
+    {
+        std::cout<<'['<<s1.top()<<"]\n";
+        s1.pop();
+    }
+}
+
+int main()
+{
+    f1();
+    return 0;
+}
